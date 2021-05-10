bc0dec3df5ddcc1ff58d84a13a0f4849976261f1
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-10 20:21:41 +0500

    Toolbar issue is fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <ThemeProvider theme={Theme}>
       <Header />
+      Hello!
     </ThemeProvider>  
   );
 }
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -18,16 +19,26 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+    barMargin: {
+        ...theme.mixins.toolbar
+    }
+}))
+
 export default function Header() {
+    const classes = useStyles()
     return(
-        <ElevationScroll>
-            <AppBar color="primary">
-                <Toolbar>
-                    <Typography variant="h3">
-                         Arc Development
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
+        <>
+            <ElevationScroll>
+                <AppBar color="primary">
+                    <Toolbar>
+                        <Typography variant="h3">
+                            Arc Development
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.barMargin}></div>
+        </>
     )
 }
