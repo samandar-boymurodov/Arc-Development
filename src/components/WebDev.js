71459b19811c77118e1b57effdd9016b5bbbbb10
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-17 19:18:41 +0500

    analytics section

--- src/components/WebDev.js
+++ src/components/WebDev.js
@@ -3,6 +3,10 @@ import { makeStyles, useTheme } from '@material-ui/styles'
 
 import backArrow from "../assets/backArrow.svg";
 import forwardArrow from "../assets/forwardArrow.svg";
+import analytics from '../assets/analytics.svg'
+import seo from '../assets/seo.svg'
+import outreach from '../assets/outreach.svg'
+import ecommerce from '../assets/ecommerce.svg'
 
 const useStyles = makeStyles(theme => ({
     mainContainer: {
@@ -49,23 +53,16 @@ export const WebDev = function() {
                 }}
             >
                 <Typography variant="h2" color="primary">
-                iOS/Android App Development
+                     Website Development
                 </Typography>
                 <Typography variant="body2" paragraph>
-                    Mobile apps allow you to take your tools on the go.
+                    Having a website is a necessity in today’s business world. They give you one central, public location to let people know who you are, what you do, and why you’re the best at it.
                 </Typography>
                 <Typography variant="body2" paragraph>
                     Whether you want an app for your customers, employees, or yourself, we can build cross-platform native solutions for any part of your business process. This opens you up to a whole new world of possibilities by taking advantage of phone features like the camera, GPS, push notifications, and more.
                 </Typography>
                 <Typography variant="body2" paragraph>
-                    Our custom solutions are designed from the ground up with your
-                    needs, wants, and goals at the core. This collaborative process
-                    produces finely tuned software that is much more effective at
-                    improving your workflow and reducing costs than generalized
-                    options.
-                </Typography>
-                <Typography variant="body2" paragraph>
-                Convenience. Connection.
+                    From simply having your hours posted to having a full fledged online store, making yourself as accessible as possible to users online drives growth and enables you to reach new customers.
                 </Typography>
             </Grid>
                 <Hidden mdDown>
@@ -75,6 +72,33 @@ export const WebDev = function() {
                     </IconButton>
                     </Grid>
                 </Hidden>
+            </Grid>
+            <Grid container item alignItems="center">
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item>
+                            <Typography variant="h4" color="primary">
+                                Analytics
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <img 
+                                src={analytics} 
+                                alt="analytics" 
+                                    style={{
+                                        marginLeft: "-2.75rem"
+                                    }}
+                                />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item style={{
+                    maxWidth: "30rem"
+                }}>
+                    <Typography variant="body2">
+                         Knowledge is power, and data is 21st Century gold. Analyzing this data can reveal hidden patterns and trends in your business, empowering you to make smarter decisions with measurable effects.
+                    </Typography>
+                </Grid>
             </Grid>
         </Grid>
     )
