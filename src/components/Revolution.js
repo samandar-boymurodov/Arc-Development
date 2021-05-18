b127ce227b5b1812c9ad9e6f1481e209800ed160
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-18 18:43:42 +0500

    align fixed

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -12,7 +12,7 @@ const defaultOptions = {
     autoplay: true,
     animationData: techAnimation,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
+        preserveAspectRatio: "xMidYMid slice",
     }
 }
 const useStyles = makeStyles(theme => ({
@@ -22,54 +22,54 @@ const useStyles = makeStyles(theme => ({
         paddingTop: "1rem",
         paddingBottom: "10rem",
         [theme.breakpoints.down("sm")]: {
-          paddingLeft: "1.5rem",
-          paddingRight: "1.5rem",
+            paddingLeft: "1.5rem",
+            paddingRight: "1.5rem",
         }
     }
 }))
 
-export const Revolution = function() {
+export const Revolution = function () {
     const classes = useStyles()
     const theme = useTheme()
 
     const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
-    return(
+    return (
         <>
-            <Grid 
-                container 
+            <Grid
+                container
                 direction="column"
-                >
+            >
                 <Grid item className={classes.mainContainer}>
-                    <Typography 
-                        variant="h2" 
+                    <Typography
+                        variant="h2"
                         color="primary"
                         align={matchesMD ? "center" : "inherit"}
                         style={{
                             fontFamily: "Pacifico"
                         }}>
-                      The Revolution
+                        The Revolution
                     </Typography>
                 </Grid>
-                <Grid 
+                <Grid
                     className={classes.mainContainer}
-                    item 
+                    item
                     container
                     alignItems="center"
                     direction={matchesMD ? "column" : "row"}>
                     <Grid item lg>
-                        <img 
-                            src={visionIcon} 
-                            alt="visionIcon" 
+                        <img
+                            src={visionIcon}
+                            alt="visionIcon"
                             style={{
                                 maxWidth: "40rem",
                                 marginRight: matchesMD ? 0 : "5rem"
                             }}
-                            />
+                        />
                     </Grid>
-                    <Grid 
-                        item 
-                        container 
-                        direction="column" 
+                    <Grid
+                        item
+                        container
+                        direction="column"
                         style={{
                             textAlign: !matchesMD ? "right" : "center",
                             maxWidth: '40rem'
@@ -82,82 +82,84 @@ export const Revolution = function() {
                         </Grid>
                         <Grid item>
                             <Typography variant="body2" paragraph>
-                              The rise of computers, and subsequently the Internet, has completely altered every aspect of human life. This has increased our comfort, broadened our connections, and reshaped how we view the world.
-                            </Typography> 
+                                The rise of computers, and subsequently the Internet, has completely altered every aspect of human life. This has increased our comfort, broadened our connections, and reshaped how we view the world.
+                            </Typography>
                             <Typography variant="body2" paragraph>
-                              What once was confined to huge rooms and teams of engineers now resides in every single one of our hands. Harnessing this unlimited potential by using it to solve problems and better lives is at the heart of everything we do.
-                            </Typography> 
+                                What once was confined to huge rooms and teams of engineers now resides in every single one of our hands. Harnessing this unlimited potential by using it to solve problems and better lives is at the heart of everything we do.
+                            </Typography>
                             <Typography variant="body2" paragraph>
-                              We want to help businesses capitalize on the latest and greatest technology. The best way to predict the future is to be the one building it, and we want to help guide the world into this next chapter of technological expansion, exploration, and innovation.
-                            </Typography> 
+                                We want to help businesses capitalize on the latest and greatest technology. The best way to predict the future is to be the one building it, and we want to help guide the world into this next chapter of technological expansion, exploration, and innovation.
+                            </Typography>
                             <Typography variant="body2" paragraph>
-                              By holding ourselves to rigorous standards and pristine quality, we can ensure you have the absolute best tools necessary to thrive in this new frontier.
-                            </Typography> 
+                                By holding ourselves to rigorous standards and pristine quality, we can ensure you have the absolute best tools necessary to thrive in this new frontier.
+                            </Typography>
                             <Typography variant="body2" paragraph>
-                              We see a future where every individual has personalized software custom tailored to their lifestyle, culture, and interests, helping them overcome life’s obstacles. Each project is a step towards that goal.
-                            </Typography> 
+                                We see a future where every individual has personalized software custom tailored to their lifestyle, culture, and interests, helping them overcome life’s obstacles. Each project is a step towards that goal.
+                            </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid 
+                <Grid
                     className={classes.mainContainer}
-                    item 
+                    item
                     container
                     alignItems="center"
                     direction={matchesMD ? "column" : "row"}>
-                    <Grid 
-                        item 
-                        container 
-                        direction="column" 
+                    <Grid
+                        item
+                        container
+                        direction="column"
                         style={{
                             textAlign: !matchesMD ? "left" : "center",
                             maxWidth: '40rem'
                         }}
                         lg
-                        >
+                    >
                         <Grid item>
                             <Typography variant="h4" color="primary">
-                             Technology
+                                Technology
                             </Typography>
                         </Grid>
                         <Grid item>
                             <Typography variant="body2" paragraph>
-                             In 2013, Facebook invented a new way of building websites. This new system, React.js, completely revolutionizes the process and practice of website development.
-                            </Typography> 
-                            <Typography variant="body2" paragraph>
-                            Instead of chaining together long individual pages, like traditional websites, React websites are built with little chunks of code called components. These components are faster, easier to maintain, and are easily reused and customized, each serving a singular purpose.
-                            </Typography> 
-                            <Typography variant="body2" paragraph>
-                            Two years later they shocked the world by releasing a similar system, React Native, for producing iOS and Android apps. Instead of having to master two completely separate development platforms, you can leverage the knowledge you already possessed from building websites and reapply it directly! This was a huge leap forward.
-                            </Typography> 
-                            <Typography variant="body2" paragraph>
-                            This technology is now being used by companies like AirBnB, Microsoft, Netflix, Pinterest, Skype, Tesla, UberEats, and when Facebook purchased Instagram large portions of it were even rebuilt using React.
-                            </Typography> 
-                            <Typography variant="body2" paragraph>
-                            Developers have since built on top of these systems by automating project setup and deployment, allowing creators to focus as much as possible on their work itself.
-                            </Typography> 
-                            <Typography variant="body2" paragraph>
-                            These technical advancements translate into savings by significantly reducing the workload and streamlining the workflow for developing new pieces of software, while also lowering the barrier to entry for mobile app development.
+                                In 2013, Facebook invented a new way of building websites. This new system, React.js, completely revolutionizes the process and practice of website development.
                             </Typography>
                             <Typography variant="body2" paragraph>
-                            This puts personalization in your pocket — faster, better, and more affordable than ever before.
+                                Instead of chaining together long individual pages, like traditional websites, React websites are built with little chunks of code called components. These components are faster, easier to maintain, and are easily reused and customized, each serving a singular purpose.
+                            </Typography>
+                            <Typography variant="body2" paragraph>
+                                Two years later they shocked the world by releasing a similar system, React Native, for producing iOS and Android apps. Instead of having to master two completely separate development platforms, you can leverage the knowledge you already possessed from building websites and reapply it directly! This was a huge leap forward.
+                            </Typography>
+                            <Typography variant="body2" paragraph>
+                                This technology is now being used by companies like AirBnB, Microsoft, Netflix, Pinterest, Skype, Tesla, UberEats, and when Facebook purchased Instagram large portions of it were even rebuilt using React.
+                            </Typography>
+                            <Typography variant="body2" paragraph>
+                                Developers have since built on top of these systems by automating project setup and deployment, allowing creators to focus as much as possible on their work itself.
+                            </Typography>
+                            <Typography variant="body2" paragraph>
+                                These technical advancements translate into savings by significantly reducing the workload and streamlining the workflow for developing new pieces of software, while also lowering the barrier to entry for mobile app development.
+                            </Typography>
+                            <Typography variant="body2" paragraph>
+                                This puts personalization in your pocket — faster, better, and more affordable than ever before.
                             </Typography>
                         </Grid>
                     </Grid>
                     <Grid item container justify="flex-end" lg>
-                        <Lottie 
+                        <Lottie
                             options={defaultOptions}
-                                style={{
-                                    maxWidth: "40rem",
-                                    margin: 0
-                                }}
-                            />
+                            style={{
+                                maxWidth: "40rem",
+                                margin: 0
+                            }}
+                        />
                     </Grid>
                 </Grid>
                 <Grid
-                    className={classes.mainContainer} 
-                    item 
-                    container 
+                    item
+                    container
+                    style={{
+                        marginBottom: "0.75rem"
+                    }}
                     justify="center">
                     <Grid item>
                         <Typography variant="h4" color="primary">
@@ -166,46 +168,46 @@ export const Revolution = function() {
                     </Grid>
                 </Grid>
                 <Grid
-                    className={classes.mainContainer} 
-                    item 
+                    className={classes.mainContainer}
+                    item
                     container
                     style={{
                         backgroundColor: "#B3B3B3",
                         height: "90rem",
                     }}>
-                    <Grid 
-                        container 
+                    <Grid
+                        container
                         direction="column"
                         lg
                         style={{
                             maxWidth: "20rem"
                         }}>
-                            <Grid item>
-                                <Typography variant="h4" style={{color: "#000"}}>
-                                    Consulation
+                        <Grid item>
+                            <Typography variant="h4" style={{ color: "#000" }}>
+                                Consulation
                                 </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="body2" 
-                                    paragraph
-                                    style={{color: "#fff"}}>
-                                    Our process begins the moment you realize you need a piece of technology for your business. Whether you already have an idea for where to start and what to do, or if you just know you want to step things up, our initial consultation will help you examine your business holistically to find the best solutions.
-                                </Typography>
-                                <Typography variant="body2" 
-                                    paragraph
-                                    style={{color: "#fff"}}>
-                                    Detailed notes will be taken on your requirements and constraints, while taking care to identify other potential areas for consideration.
-                                </Typography>
-                                <Typography variant="body2" 
-                                    paragraph
-                                    style={{color: "#fff"}}>
-                                    Cutting-edge advancements in machine learning like object detection and natural language processing allow computers to do things previously unimaginable, and our expertise and intuition will help usher you into this new future of possibilities.
-                                </Typography>
-                            </Grid>
                         </Grid>
-                        <Grid item lg container alignItems="center" justify="center">
-                            <img src={handshake} alt="handshake" />
+                        <Grid item>
+                            <Typography variant="body2"
+                                paragraph
+                                style={{ color: "#fff" }}>
+                                Our process begins the moment you realize you need a piece of technology for your business. Whether you already have an idea for where to start and what to do, or if you just know you want to step things up, our initial consultation will help you examine your business holistically to find the best solutions.
+                                </Typography>
+                            <Typography variant="body2"
+                                paragraph
+                                style={{ color: "#fff" }}>
+                                Detailed notes will be taken on your requirements and constraints, while taking care to identify other potential areas for consideration.
+                                </Typography>
+                            <Typography variant="body2"
+                                paragraph
+                                style={{ color: "#fff" }}>
+                                Cutting-edge advancements in machine learning like object detection and natural language processing allow computers to do things previously unimaginable, and our expertise and intuition will help usher you into this new future of possibilities.
+                                </Typography>
                         </Grid>
+                    </Grid>
+                    <Grid item lg container alignItems="center" justify="center">
+                        <img src={handshake} alt="handshake" />
+                    </Grid>
                 </Grid>
             </Grid>
             <CallToAction />
