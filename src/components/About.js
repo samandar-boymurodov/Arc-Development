269e44c146ec5d1725e436a34fcddd2a3b6337a4
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-20 20:41:44 +0500

    some changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,16 @@
 import { Grid, Typography, Avatar, useMediaQuery } from '@material-ui/core'
 import { useTheme, makeStyles } from '@material-ui/styles'
+import history from '../assets/history.svg'
 
 const useStyles = makeStyles(theme => ({
+    rowContainer: {
+        paddingLeft: "5rem",
+        paddingRight: "5rem",
+        [theme.breakpoints.down("sm")]: {
+            paddingLeft: "1.5rem",
+            paddingRight: "1.5rem",
+        }
+    },
     mission: {
         fontWeight: 300,
         fontStyle: "italic",
@@ -16,12 +25,18 @@ export const About = function () {
 
     return(
         <Grid container direction="column">
-            <Grid item>
+            <Grid 
+                item
+                className={classes.rowContainer}>
                 <Typography variant="h2" color="primary">
                     About Us
                 </Typography>
             </Grid>
-            <Grid item container justify="center">
+            <Grid 
+                item 
+                container 
+                justify="center"
+                className={classes.rowContainer}>
                 <Grid 
                     item
                     >
@@ -34,6 +49,62 @@ export const About = function () {
                     </Typography>
                 </Grid>
             </Grid>
+            <Grid
+                item
+                container 
+                className={classes.rowContainer}
+                justify="space-around"
+                >
+                    <Grid item>
+                        <Grid 
+                            item 
+                            container 
+                            direction="column"
+                            style={{
+                                maxWidth: "35rem"
+                            }}
+                            >
+                            <Grid item>
+                                <Typography variant="h4" color="primary">
+                                    History
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography 
+                                    variant="body2"
+                                    style={{
+                                        fontWeight: 700,
+                                        fontStyle: "italic"
+                                    }}>
+                                        We're the new kid on the block
+                                    </Typography> 
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body2" paragraph>
+                                    Founded in 2019, we’re ready to get our hands on the world’s business problems.
+                                </Typography>
+                                <Typography variant="body2" paragraph>
+                                    it all started with one question: Why aren’t all businesses using available technology? There are many different answers to that question: economic barriers, social barriers, educational barriers, and sometimes institutional barriers.
+                                </Typography>
+                                <Typography variant="body2" paragraph>
+                                We aim to be a powerful force in overcoming these obstacles. Recent developments in software engineering and computing power, compounded by the proliferation of smart phones, has opened up infinite worlds of possibility. Things that have always been done by hand can now be done digitally and automatically, and completely new methods of interaction are created daily. Taking full advantage of these advancements is the name of the game.
+                                </Typography>
+                                <Typography variant="body2" paragraph>
+                                    All this change can be a lot to keep up with, and that’s where we come in.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item style={{backgroundColor: "red"}}>
+                        <img 
+                            src={history} 
+                            alt="history" 
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+                    </Grid>
+                </Grid>
         </Grid>
     )
 }
