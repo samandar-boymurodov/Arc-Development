b69e30a43212811c3cdb9c0c8c4175d421776f54
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-21 15:01:22 +0500

    about page finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
-import { Grid, Typography, useMediaQuery, Avatar } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  useMediaQuery,
+  Avatar,
+  Hidden,
+} from "@material-ui/core";
 import { useTheme, makeStyles } from "@material-ui/styles";
 import history from "../assets/history.svg";
 
 import profile from "../assets/founder.jpg";
 import yearBook from "../assets/yearbook.svg";
 import puppy from "../assets/puppy.svg";
+import { CallToAction } from "./UI/CallToAction.js";
 
 const useStyles = makeStyles((theme) => ({
   rowContainer: {
@@ -18,6 +25,10 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     height: "25rem",
     width: "25rem",
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: 300,
+      maxHeight: 300,
+    },
   },
   mission: {
     fontWeight: 300,
@@ -26,25 +37,46 @@ const useStyles = makeStyles((theme) => ({
     lineHeight: 1.4,
     fontSize: "1.5rem",
   },
+  caption: {
+    fontSize: "1rem",
+    fontWeight: 300,
+    color: theme.palette.text.secondary,
+  },
 }));
 
 export const About = function () {
   const classes = useStyles();
+  const theme = useTheme();
 
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <Grid container direction="column">
       <Grid item className={classes.rowContainer}>
-        <Typography variant="h2" color="primary">
+        {" "}
+        {/* --- About Us Title --- */}
+        <Typography
+          variant="h2"
+          color="primary"
+          align={matchesMD ? "center" : undefined}
+          style={{
+            marginTop: matchesMD ? "1rem" : "2rem",
+          }}
+        >
           About Us
         </Typography>
       </Grid>
       <Grid item container justify="center" className={classes.rowContainer}>
+        {" "}
+        {/* --- Mission --- */}
         <Grid item>
           <Typography
             variant="h4"
             align="center"
             color="primary"
             className={classes.mission}
+            style={{
+              marginTop: "3rem",
+            }}
           >
             Whether it be person to person, business to consumer, or an
             individual to their interests, technology is meant to bring us
@@ -54,11 +86,16 @@ export const About = function () {
           </Typography>
         </Grid>
       </Grid>
+      {/* --- History --- */}
       <Grid
         item
         container
         className={classes.rowContainer}
         justify="space-around"
+        style={{
+          marginTop: "10rem",
+          marginBottom: "10rem",
+        }}
       >
         <Grid item>
           <Grid
@@ -67,6 +104,7 @@ export const About = function () {
             direction="column"
             style={{
               maxWidth: "35rem",
+              textAlign: matchesMD ? "center" : undefined,
             }}
           >
             <Grid item>
@@ -122,6 +160,7 @@ export const About = function () {
           />
         </Grid>
       </Grid>
+      {/* --- Team --- */}
       <Grid
         item
         container
@@ -145,6 +184,111 @@ export const About = function () {
         <Grid item>
           <Avatar src={profile} className={classes.avatar} />
         </Grid>
+        <Grid item>
+          <Hidden lgUp>
+            <Grid
+              item
+              container
+              lg
+              style={{ textAlign: "center", padding: "1.25rem" }}
+            >
+              <Typography variant="body2" paragraph>
+                I taught myself basic coding from a library book in third grade,
+                and ever since then my passion has solely been set on learning —
+                learning about computers, learning mathematics and philosophy,
+                studying design, always just learning.
+              </Typography>
+              <Typography variant="body2">
+                Now I’m ready to apply everything I’ve learned, and to help
+                others with the intuition I have developed. I'm currently
+                teaching a course about building responsive modern user
+                interfaces on Udemy.com as well as beginning work on my first
+                machine learning mobile application.
+              </Typography>
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Grid>{" "}
+      {/* --- Images --- */}
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{
+          marginBottom: "15rem",
+        }}
+      >
+        <Grid
+          item
+          container
+          direction="column"
+          lg
+          alignItems={matchesMD ? "center" : undefined}
+          style={{
+            marginBottom: matchesMD ? "2.5rem" : 0,
+          }}
+        >
+          <Grid item>
+            <img
+              src={yearBook}
+              alt="yearBook"
+              style={{ maxWidth: matchesMD ? 300 : undefined }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="caption" className={classes.caption}>
+              a page from my Sophomore yearbook
+            </Typography>
+          </Grid>
+        </Grid>
+        <Hidden mdDown>
+          <Grid
+            item
+            container
+            lg
+            style={{ textAlign: "center", padding: "1.25rem" }}
+          >
+            <Typography variant="body2" paragraph>
+              I taught myself basic coding from a library book in third grade,
+              and ever since then my passion has solely been set on learning —
+              learning about computers, learning mathematics and philosophy,
+              studying design, always just learning.
+            </Typography>
+            <Typography variant="body2">
+              Now I’m ready to apply everything I’ve learned, and to help others
+              with the intuition I have developed. I'm currently teaching a
+              course about building responsive modern user interfaces on
+              Udemy.com as well as beginning work on my first machine learning
+              mobile application.
+            </Typography>
+          </Grid>
+        </Hidden>
+        <Grid
+          item
+          container
+          direction="column"
+          lg
+          alignItems="flex-end"
+          alignItems={matchesMD ? "center" : "flex-end"}
+        >
+          <Grid item>
+            <img
+              src={puppy}
+              alt="puppy"
+              style={{
+                maxWidth: matchesMD ? 300 : undefined,
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="caption" className={classes.caption}>
+              my miniature dapple dachshund, Sterling
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction />
       </Grid>
     </Grid>
   );
