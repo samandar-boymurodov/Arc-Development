7ba0ee544f21dec934b4f482c6cb2966b8bd1a52
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-23 11:11:37 +0500

    change style of inputs

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -1,68 +1,77 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export const Theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: "#1769aa"
-        },
-        secondary: {
-            main: "#ff9800",
-            light: "#ffa726"
-        },
-        text: {
-            secondary: "#868686"
-        }
+  palette: {
+    primary: {
+      main: "#1769aa",
     },
-    typography: {
-        tab: {
-            fontFamily: "Raleway",
-            textTransform: "none",
-            fontWeight: "700",
-            fontSize: "1rem"
-        },
-        estimate: {
-            fontFamily: "Pacifico",
-            fontSize: "1rem",
-            textTransform: "none",
-            color: "white"
-        },
-        h2: {
-            fontSize: "2.5em",
-            fontWeight: 700,
-            fontFamily: "Raleway",
-            lineHeight: 1.5
-        },
-        h4: {
-            fontSize: "1.75rem",
-            fontWeight: 700,
-            fontFamily: "Raleway"
-        },
-        subtitle1: {
-            fontSize: "1.25rem",
-            fontWeight: 300
-        },
-        learnMoreButton: {
-            color: "#1769aa",
-            borderColor: "#1769aa",
-            fontFamily: "Roboto",
-            borderWidth: 2,
-            fontSize: "0.8rem",
-            fontWeight: "bold",
-            borderRadius: 50
-        },
-        h3: {
-            fontFamily: "Pacifico",
-            fontSize: "2.5rem"
-        },
-        subtitle2: {
-            color: "white",
-            fontSize: "1.25rem",
-            fontWeight: 300
-        },
-        body2: {
-            fontSize: "1.25rem",
-            fontWeight: 300,
-            color: "#868686"
-        }
-    }
-})
+    secondary: {
+      main: "#ff9800",
+      light: "#ffa726",
+    },
+    text: {
+      secondary: "#868686",
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: "700",
+      fontSize: "1rem",
+    },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white",
+    },
+    h2: {
+      fontSize: "2.5em",
+      fontWeight: 700,
+      fontFamily: "Raleway",
+      lineHeight: 1.5,
+    },
+    h4: {
+      fontSize: "1.75rem",
+      fontWeight: 700,
+      fontFamily: "Raleway",
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+    },
+    learnMoreButton: {
+      color: "#1769aa",
+      borderColor: "#1769aa",
+      fontFamily: "Roboto",
+      borderWidth: 2,
+      fontSize: "0.8rem",
+      fontWeight: "bold",
+      borderRadius: 50,
+    },
+    h3: {
+      fontFamily: "Pacifico",
+      fontSize: "2.5rem",
+    },
+    subtitle2: {
+      color: "white",
+      fontSize: "1.25rem",
+      fontWeight: 300,
+    },
+    body2: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: "#868686",
+    },
+  },
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        fontSize: "1rem",
+        color: "#1769aa",
+      },
+    },
+    MuiInput: {},
+  },
+});
